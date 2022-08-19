728adb40abf13dbf25109c1278bea79098190a3c
louis1706 <louismonneyron5@yahoo.com>
2022-08-19 11:23:42 +0200

    Fix CustomIntercom bug

--- Patches/NameFormaterPatche.cs
+++ Patches/NameFormaterPatche.cs
@@ -6,22 +6,23 @@ using UnityEngine;
 
 namespace FacilityManagement.Patches
 {
-    [HarmonyPatch(typeof(ServerConsole), nameof(ServerConsole.Start))]
-
+    [HarmonyPatch(typeof(Intercom), nameof(Intercom.Start))]
     public class NameFormaterPatche
     {
         public static void Postfix()
         {
             try
             {
+                if (ServerConsole.singleton.NameFormatter.Commands.ContainsKey("intercom_speech_remaining_time"))
+                    return;
                 Utils.CommandInterpolation.InterpolatedCommandFormatter interpolatedCommandFormatter = ServerConsole.singleton.NameFormatter;
                 //Int
                 interpolatedCommandFormatter.Commands.Add("intercom_speech_remaining_time", (List<string> args) => Mathf.CeilToInt(Exiled.API.Features.Intercom.SpeechRemainingTime).ToString());
                 interpolatedCommandFormatter.Commands.Add("intercom_remaining_cooldown", (List<string> args) => Mathf.CeilToInt(Intercom.host.remainingCooldown).ToString());
                 //String
-                interpolatedCommandFormatter.Commands.Add("intercom_speaker_nickname", (List<string> args) => Exiled.API.Features.Intercom.Speaker.Nickname.ToString());
+                interpolatedCommandFormatter.Commands.Add("intercom_speaker_nickname", (List<string> args) => $"{Player.Get(Intercom.host?.speaker)?.Nickname}");
                 //Bool
-                interpolatedCommandFormatter.Commands.Add("intercom_is_in_use", (List<string> args) => (Intercom.host.speaker is not null).ToString());
+                interpolatedCommandFormatter.Commands.Add("intercom_is_in_use", (List<string> args) => (Intercom.host?.speaker is not null).ToString());
                 interpolatedCommandFormatter.Commands.Add("intercom_is_admin_speaking", (List<string> args) => Intercom.AdminSpeaking.ToString());
                 interpolatedCommandFormatter.Commands.Add("intercom_bypass_speaking", (List<string> args) => Intercom.host.bypassSpeaking.ToString());
                 interpolatedCommandFormatter.Commands.Add("intercom_mute_player_speak", (List<string> args) => Intercom.host.Muted.ToString());
@@ -30,7 +31,7 @@ namespace FacilityManagement.Patches
             }
             catch (Exception ex)
             {
-                Log.Error($"ServerConsole::Start Postfix : {ex}\n {ex.StackTrace}");
+                Log.Error($"Intercom::Start Postfix : {ex}\n {ex.StackTrace}");
             }
         }
     }
