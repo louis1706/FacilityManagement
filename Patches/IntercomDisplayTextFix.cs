e6b1b8d004223e247eb165e8b530824487f93863
louis1706 <louismonneyron5@yahoo.com>
2022-09-08 17:26:34 +0200

    Using Transpiler
    - Using transpiler is GigaChad
    - Remove all Prefix and Postfix
    - No change for user (if you find a change with this commit it's a bug)

--- Patches/IntercomDisplayTextFix.cs
+++ Patches/IntercomDisplayTextFix.cs
@@ -1,16 +1,56 @@
-﻿using HarmonyLib;
+﻿using Exiled.Events.EventArgs;
+using HarmonyLib;
+using LightContainmentZoneDecontamination;
+using NorthwoodLib.Pools;
+using System.Collections.Generic;
+using System.Reflection.Emit;
+using static HarmonyLib.AccessTools;
 
 namespace FacilityManagement.Patches
 {
+#pragma warning disable IDE0060 // Supprimer le paramètre inutilisé
+
     [HarmonyPatch(typeof(Exiled.API.Features.Intercom), nameof(Exiled.API.Features.Intercom.DisplayText), MethodType.Setter)]
     public static class CommandIntercomTextSetterFix
     {
-        public static void Prefix(ref string value) => FacilityManagement.Singleton.CustomText = value;
+        public static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions, ILGenerator generator)
+        {
+            List<CodeInstruction> newInstructions = ListPool<CodeInstruction>.Shared.Rent();
+
+            newInstructions.AddRange(new CodeInstruction[]
+            {
+                new(OpCodes.Ldsfld, Field(typeof(FacilityManagement),nameof(FacilityManagement.Singleton))),
+                new(OpCodes.Ldarg_0),
+                new(OpCodes.Ldind_Ref),
+                new(OpCodes.Stfld, Field(typeof(FacilityManagement),nameof(FacilityManagement.Singleton.CustomText))),
+                new(OpCodes.Ret),
+            });
+
+            for (int z = 0; z < newInstructions.Count; z++)
+                yield return newInstructions[z];
+
+            ListPool<CodeInstruction>.Shared.Return(newInstructions);
+        }
     }
 
     [HarmonyPatch(typeof(Exiled.API.Features.Intercom), nameof(Exiled.API.Features.Intercom.DisplayText), MethodType.Getter)]
     public static class CommandIntercomTextGetterFix
     {
-        public static void Prefix(ref string __result) => __result = FacilityManagement.Singleton.CustomText;
+        public static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions, ILGenerator generator)
+        {
+            List<CodeInstruction> newInstructions = ListPool<CodeInstruction>.Shared.Rent(instructions);
+
+            newInstructions.AddRange(new CodeInstruction[]
+            {
+                new(OpCodes.Ldsfld, Field(typeof(FacilityManagement),nameof(FacilityManagement.Singleton))),
+                new(OpCodes.Ldfld, Field(typeof(FacilityManagement),nameof(FacilityManagement.Singleton.CustomText))),
+                new(OpCodes.Ret),
+            });
+
+            for (int z = 0; z < newInstructions.Count; z++)
+                yield return newInstructions[z];
+
+            ListPool<CodeInstruction>.Shared.Return(newInstructions);
+        }
     }
 }
--- Patches/IntercomPatch.cs
+++ Patches/IntercomPatch.cs
@@ -2,6 +2,11 @@
 using System;
 using Exiled.API.Features;
 using UnityEngine;
+using NorthwoodLib.Pools;
+using System.Collections.Generic;
+using System.Reflection.Emit;
+using static HarmonyLib.AccessTools;
+using Mono.Cecil.Cil;
 
 namespace FacilityManagement.Patches
 {
@@ -10,10 +15,33 @@ namespace FacilityManagement.Patches
         [HarmonyPatch(typeof(Intercom), nameof(Intercom.IntercomState), MethodType.Setter)]
         public static class CommandIntercomTextSetterFix
         {
-            public static bool Prefix(Intercom __instance, ref Intercom.State value) 
+            public static void Postfix(Intercom __instance, ref Intercom.State value)
             {
                 __instance.Network_state = SetContent(__instance, value);
-                return false; 
+                return;
+            }
+            public static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions, ILGenerator generator)
+            {
+                Label returnLabel = generator.DefineLabel();
+
+                List<CodeInstruction> newInstructions = ListPool<CodeInstruction>.Shared.Rent();
+
+                newInstructions.AddRange(new CodeInstruction[]
+                {
+                    new(OpCodes.Nop),
+                    new(OpCodes.Ldarg_0),
+                    new(OpCodes.Ldarg_0),
+                    new(OpCodes.Ldarg_1),
+                    new(OpCodes.Ldind_U1),
+                    new(OpCodes.Call, Method(typeof(IntercomUpdateTextPatch), nameof(SetContent))),
+                    new(OpCodes.Callvirt, PropertySetter(typeof(Intercom), nameof(Intercom.Network_state))), // Set string
+                    new CodeInstruction(OpCodes.Ret),
+                });
+
+                for (int z = 0; z < newInstructions.Count; z++)
+                    yield return newInstructions[z];
+
+                ListPool<CodeInstruction>.Shared.Return(newInstructions);
             }
         }
         internal static Intercom.State SetContent(Intercom intercom, Intercom.State state)
